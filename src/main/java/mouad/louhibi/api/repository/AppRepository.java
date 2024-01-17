3a9651efe24c035d42f39724b05e0944ab85b742
Mouad <mouadlouhibi2000@gmail.com>
2024-01-17 03:02:31 +0100

    AppRepository

--- src/main/java/mouad/louhibi/api/repository/AppRepository.java
+++ src/main/java/mouad/louhibi/api/repository/AppRepository.java
@@ -38,4 +38,40 @@ public class AppRepository {
 
         return simpleJdbcCall.execute(in);
     }
+
+    public Map<String, Object> updateStudentRecord(AppModel model) {
+        simpleJdbcCall = new SimpleJdbcCall(jdbcTemplate).withProcedureName("update_student");
+        SqlParameterSource in = new MapSqlParameterSource()
+                .addValue("student_fname", model.getStudent().getFname())
+                .addValue("student_lname", model.getStudent().getLname())
+                .addValue("student_dob", model.getStudent().getDob())
+                .addValue("subject_name", model.getSubject().getName())
+                .addValue("subject_section", model.getSubject().getSection())
+                .addValue("subject_teacher", model.getSubject().getTeacher());
+
+        return simpleJdbcCall.execute(in);
+    }
+
+    public Map<String, Object> getStudent(Integer id) {
+        simpleJdbcCall = new SimpleJdbcCall(jdbcTemplate).withProcedureName("get_student");
+        SqlParameterSource in = new MapSqlParameterSource()
+                .addValue("id", id);
+
+        return simpleJdbcCall.execute(in);
+    }
+
+    public Map<String, Object> getStudents() {
+        simpleJdbcCall = new SimpleJdbcCall(jdbcTemplate).withProcedureName("get_students");
+        SqlParameterSource in = new MapSqlParameterSource();
+
+        return simpleJdbcCall.execute(in);
+    }
+
+    public Map<String, Object> deleteStudent(Integer id) {
+        simpleJdbcCall = new SimpleJdbcCall(jdbcTemplate).withProcedureName("delete_student");
+        SqlParameterSource in = new MapSqlParameterSource()
+                .addValue("id", id);
+
+        return simpleJdbcCall.execute(in);
+    }
 }
